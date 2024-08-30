7db8061efcf45a8ac0d62ca8d9249beba87e3d03
Mannmeet <manmeet.a.jb@gmail.com>
2024-08-31 00:11:09 +0530

    Terraform code

--- infra-create/data.tf
+++ infra-create/data.tf
@@ -6,11 +6,11 @@ data "aws_ami" "rhel9" {
   owners = ["973714476881"]
 }
 
-data "terraform_remote_state" "remotetf" {
-  backend = "s3"
-  config = {
-    bucket = "tool-infra-create"
-    key    = "remotetf/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# data "terraform_remote_state" "remotetf" {
+#   backend = "s3"
+#   config = {
+#     bucket = "tool-infra-create"
+#     key    = "remotetf/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
--- tfstate.tf
+++ tfstate.tf
@@ -5,3 +5,11 @@
 #     region = "us-east-1"
 #   }
 # }
+data "terraform_remote_state" "remotetf" {
+  backend = "s3"
+  config = {
+    bucket = "tool-infra-create"
+    key    = "remotetf/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
