70ad305d241b3532966377d37fd2a9dd98fdb707
Mannmeet <manmeet.a.jb@gmail.com>
2024-08-30 23:17:22 +0530

    Terraform code

--- variables.tf
+++ variables.tf
@@ -1,30 +1,33 @@
 variable "tools" {
-  default = [
+  type = map(object({
+    instance_type  = string
+    policy_name    = list(string)
+    ports          = map(number)  # Assuming ports are a map with port names as keys and port numbers as values
+    volume_size    = number
+  }))
+  default =
     {
-      github_runner = {
-        instance_type = "t3.small"
-        policy_name = [
-          "AdministratorAccess"
-        ]
-        ports = {}
-        volume_size = 20
-      }
-      vault = {
-        instance_type = "t3.small"
-        policy_name = []
-        ports = {
-          vault = 8200
-        }
-        volume_size = 20
-      }
-      workstation = {
-        instance_type = "t3.small"
-        policy_name = []
-        ports = {}
-        volume_size = 20
-      }
+    github_runner = {
+      instance_type = "t3.small"
+      policy_name   = ["AdministratorAccess"]
+      ports          = {}
+      volume_size    = 20
     }
-  ]
+    vault = {
+      instance_type = "t3.small"
+      policy_name   = []
+      ports          = {
+        vault = 8200
+      }
+      volume_size    = 20
+    }
+    workstation = {
+      instance_type = "t3.small"
+      policy_name   = []
+      ports          = {}
+      volume_size    = 20
+    }
+    }
 }
 
 variable "hosted_zone_id" {
@@ -33,9 +36,6 @@ variable "hosted_zone_id" {
 variable "vpc_id" {
   default = {}
 }
-# variable "AWS_ACCESS_KEY_ID" {
-#   default = ${{ secret.AWS_ACCESS_KEY_ID }}
-# }
-# variable "AWS_SECRET_ACCESS_KEY" {
-#   default = ${{ secret.AWS_SECRET_ACCESS_KEY }}
-# }
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
