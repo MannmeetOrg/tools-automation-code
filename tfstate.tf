bf1f98c6b47ae088581d658c8eb7a54ebfe7f80a
Mannmeet <manmeet.a.jb@gmail.com>
2024-08-31 00:33:40 +0530

    Terraform code

--- tfstate.tf
+++ tfstate.tf
@@ -1,15 +1,15 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "tool-infra-create"
-#     key    = "tools/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
-data "terraform_remote_state" "remotetf" {
-  backend = "s3"
-  config = {
+terraform {
+  backend "s3" {
     bucket = "tool-infra-create"
-    key    = "remotetf/terraform.tfstate"
+    key    = "tools/terraform.tfstate"
     region = "us-east-1"
   }
 }
+# data "terraform_remote_state" "remotetf" {
+#   backend = "s3"
+#   config = {
+#     bucket = "tool-infra-create"
+#     key    = "remotetf/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
