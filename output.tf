be5b0d37b360bfb965bcd1b39420d16b267e8392
Mannmeet <manmeet.a.jb@gmail.com>
2024-08-31 18:38:21 +0530

    Terraform code

--- output.tf
+++ output.tf
@@ -2,10 +2,10 @@ output "var_tools" {
   value = var.tools
 }
 
-output "access" {
-  value = AWS_ACCESS_KEY_ID
-}
-
-output "key" {
-  value = AWS_SECRET_ACCESS_KEY
-}
+# output "access" {
+#   value = AWS_ACCESS_KEY_ID
+# }
+#
+# output "key" {
+#   value = AWS_SECRET_ACCESS_KEY
+# }
