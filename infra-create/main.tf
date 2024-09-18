bd0fc65846fd10441cf88cdd72eb1e89e4c404d4
Mannmeet <manmeet.a.jb@gmail.com>
2024-09-18 21:35:40 +0530

    Terraform code

--- infra-create/main.tf
+++ infra-create/main.tf
@@ -3,6 +3,15 @@ resource "aws_instance" "tool" {
   instance_type           = var.instance_type
   vpc_security_group_ids  = [aws_security_group.infra-sg.id]
   iam_instance_profile    = aws_iam_instance_profile.instance-profile.name
+
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type = "persistent"
+    }
+  }
+
 }
 resource "aws_route53_record" "public_record" {
   zone_id = var.hosted_zone_id
