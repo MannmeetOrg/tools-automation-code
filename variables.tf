884ce31f113a8b1dd6c62cacaf8f536225f71abf
MannuA19 <mannmeet.ahir@gmail.com>
2024-12-06 19:43:08 +0530

    Terraform code Modified

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "tools" {
   default = {
     github_runner = {
-      instance_type = "t2.micro"
+      instance_type = "t2.small"
       policy_name   = ["AdministratorAccess"]
       ports          = {}
       volume_size    = 20
     }
     vault = {
-      instance_type = "t2.micro"
+      instance_type = "t2.small"
       policy_name   = []
       ports          = {
         vault = 8200
